22b478e139450f28de85e607c7478c0ac9944ccf
siegfile <dkruchek@kse.org.ua>
2022-12-14 18:53:00 +0200

    final_task_3

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file', type=argparse.FileType('r'))
-parser.add_argument('-medals', dest='medals', nargs=2)
+parser.add_argument('-medals', nargs=2)
 parser.add_argument('-output', '--output', type=argparse.FileType('w'))
 parser.add_argument('-overall', nargs='*')
+
 
 def medals(line, user_country, user_year, dict):
     sep_info = line.split('\t')
@@ -19,21 +20,44 @@ def medals(line, user_country, user_year, dict):
     return dict
 
 
+def overall(line, dict):
+    sep_info = line.split('\t')
+    for i in dict:
+        if i == sep_info[6] and sep_info[14] != 'NA\n':
+            dict[i] = dict[i] + sep_info[9] + ';'
+
+    return dict
+
+
 args = parser.parse_args()
 
 with args.file as file:
     next_line = file.readline()
     sport_dict = {'index': 0, 'sportsman_info': '', 'gold': 0, 'silver': 0, 'bronze': 0}
+    overall_medals = dict.fromkeys(args.overall, '')
     while next_line != '':
         next_line = file.readline()
         if args.medals is not None and sport_dict['index'] < 10:
             sport_dict = medals(next_line, args.medals[0], args.medals[1], sport_dict)
+        if args.overall is not None and next_line != '':
+          overall_medals = overall(next_line, overall_medals)
     if args.medals is not None: print(sport_dict['gold'], sport_dict['silver'], sport_dict['bronze'])
     if args.output is not None:
         medalists = sport_dict['sportsman_info'].split(';')
-        for name in medals:
+        for name in medalists:
             args.output.writelines(name)
         args.output.writelines(str(sport_dict['gold']) + ' ' + str(sport_dict['silver']) + ' ' + str(sport_dict['bronze']) + ' ')
     if args.medals is not None and sport_dict['index'] == 0:
         print('Введена країна не існує або у введений рік не проводилась олімпіада')
+    if args.overall is not None:
+        for i in overall_medals:
+            top_medals = 0
+            top_year = 0
+            overall_years = overall_medals[i].split(';')
+            print(overall_medals[i])
+            for j in range(2022):
+                if overall_years.count(str(j)) > top_medals:
+                    top_medals = overall_years.count(str(j))
+                    top_year = j
+            print(i, top_year, top_medals)
 
